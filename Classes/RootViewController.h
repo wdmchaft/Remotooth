0694de1a2b6fb6ebcc5e4b039828c7dc0a802d6e
topherfangio <fangiotophia@gmail.com>
2009-07-06 22:56:35 -0500

    Initial commit

---
+++ Classes/RootViewController.h
@@ -0,0 +1,12 @@
+//
+//  RootViewController.h
+//  Remotooth
+//
+//  Created by Topher Fangio on 6/23/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+@interface RootViewController : UITableViewController {
+}
+
+@end
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Remotooth
+//
+//  Created by Topher Fangio on 6/23/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
